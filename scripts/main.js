2e690d2381105714147a3e63dfe514d45579bcc8
Danique Lammertink <danique.lammertink@hva.nl>
2022-10-31 19:21:49 +0100

    💄Added Sass
    Im gonna use sass styling instead of css

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 // Our bundler automatically creates styling when imported in the main JS file!
-import '../styles/style.css'
+import '../styles/style.scss'
 
 // We can use node_modules directely in the browser!
 import * as d3 from 'd3';
