5281641107a3b14c4bcdbe117b69fd7516df07dc
Danique Lammertink <danique.lammertink@hva.nl>
2022-11-10 09:54:05 +0100

    💄 SCSS improvements

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ import '../styles/style.scss'
 // We can use node_modules directely in the browser!
 import * as d3 from 'd3';
 
-document.querySelector(".btn").addEventListener("click", () => {
+document.querySelector(".table").addEventListener("click", () => {
     document.querySelector(".table").classList.toggle("change");
   });
  
