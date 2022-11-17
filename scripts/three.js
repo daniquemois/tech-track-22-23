db44ffd9a53d392b4705ac5dc64fbd6d0a42e60c
Danique Lammertink <danique.lammertink@hva.nl>
2022-11-17 08:36:29 +0100

    💩 Three js is not working yet

--- scripts/three.js
+++ scripts/three.js
@@ -1,24 +1,65 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const scene = new THREE.Scene();
+	const scene = new THREE.Scene();
+			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+			const renderer = new THREE.WebGLRenderer();
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			document.body.appendChild( renderer.domElement );
 
-const loader = new GLTFLoader();
+			const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+			const cube = new THREE.Mesh( geometry, material );
+			scene.add( cube );
 
-loader.load( '../3dobjects/wooden_table/scene.gltf', function ( gltf ) {
+			camera.position.z = 5;
 
-	scene.add( gltf.scene );
+            const loader = new GLTFLoader();
 
-}, undefined, function ( error ) {
+            loader.load( '../3dobjects/wooden_table/scene.gltf', function ( gltf ) {
+            
+                scene.add( gltf.scene );
+                gltf.textures;
+            
+            }, undefined, function ( error ) {
+            
+                console.error( error );
+            
+            } );
+            
+			function animate() {
+				requestAnimationFrame( animate );
 
-	console.error( error );
+				cube.rotation.x += 0.01;
+				cube.rotation.y += 0.01;
 
-} );
+				renderer.render( scene, camera );
+			};
 
-camera.position.z = 5;
+			animate();
 
 
-scene.add( loader );
-animate();
+
+
+// const scene = new THREE.Scene();
+
+// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+// const loader = new GLTFLoader();
+
+// loader.load( '../3dobjects/wooden_table/scene.gltf', function ( gltf ) {
+
+// 	scene.add( gltf.scene );
+
+// }, undefined, function ( error ) {
+
+// 	console.error( error );
+
+// } );
+
+// camera.position.z = 5;
+
+
+// scene.add( loader );
+// animate();
